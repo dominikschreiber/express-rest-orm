aad2a3e5fb4f085db5bdd9d4d4be7e913b18e82f
Dominik Schreiber <dschreiber@kellerkinder.de>
2015-05-14 20:34:58 +0200

    added PUT /:resource bulk update, added tests for /_errors/

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var expressRestOrm = require('../lib/index')
+  , expressRestOrmErrors = require('../lib/errors')
+  , Q = require('q')
   , supertest = require('supertest')
   , assert = require('assert')
   , express = require('express')
@@ -27,56 +29,33 @@ var expressRestOrm = require('../lib/index')
               type: Sequelize.STRING
         }
     })
-  , Test = orm.define('test', {
-        user: {
-            type: Sequelize.INTEGER // => User foreign key
-        },
-        test: {
-            type: Sequelize.INTEGER
-        }
-    })
-  , models = [User, Test]
-  , users = []
-  , tests = []
+  , models = [User]
+  , users = {
+        dominik: {id: 1, givenname: 'Dominik', lastname: 'Schreiber'},
+        hanna: {id: 2, givenname: 'Hanna', lastname: 'Schreiber'}
+    }
 
   , app = false
   , request = false;
 
+function clean(data) {
+    return _.omit(data, ['createdAt', 'updatedAt']);
+}
+
 describe('', function() {
     beforeEach(function(done) {
         orm.sync({force: true}).then(function() {
-            users = [];
-            tests = [];
+            Q.all(_.map(_.values(users), function(user) {
+                return User.create(_.omit(user, ['id']));
+            })).then(function() {
+                app = express();
+                app.use(bodyparser.json());
+                app.use('/', expressRestOrm(models));
 
-            User.create({
-                givenname: 'Dominik',
-                lastname: 'Schreiber'
-            }).then(function(user) {
-                users.push(user);
-                Test.create({
-                    user: user.dataValues.id,
-                    test: 1
-                }).then(tests.push);
+                request = supertest(app);
+                
+                done();
             });
-
-            User.create({
-                givenname: 'Hanna',
-                lastname: 'Schreiber'
-            }).then(function(user) {
-                users.push(user);
-                Test.create({
-                    user: user.dataValues.id,
-                    test: 2
-                }).then(tests.push);
-            });
-
-            app = express();
-            app.use(bodyparser.json());
-            app.use('/', expressRestOrm(models));
-
-            request = supertest(app);
-            
-            done();
         });
     });
 
@@ -104,10 +83,12 @@ describe('', function() {
                 .expect(200)
                 .end(function(err, result) {
                     if (err) { done(err); }
-                    assert.deepEqual(result.body, _.map(_.range(1, users.length + 1), function(i) {
-                        return '/users/' + i;
-                    }));
-                    done();
+                    User.count().then(function(len) {
+                        assert.deepEqual(result.body, _.map(_.range(1, len + 1), function(i) {
+                            return '/users/' + i;
+                        }));
+                        done();
+                    });
                 });
         });
     });
@@ -160,26 +141,63 @@ describe('', function() {
                 .expect(200)
                 .end(function(err, result) {
                     if (err) { done(err); }
-                    assert.deepEqual(result.body, _.map(users, function(user) {
-                        return _.omit(user.dataValues, ['createdAt', 'updatedAt']);
-                    }));
-                    done();
+                    User.findAll().then(function(results) {
+                        assert.deepEqual(result.body, _.map(results, function(r) {
+                            return clean(r.dataValues);
+                        }));
+                        done();
+                    });
                 });
         });
     });
 
     describe('  POST /:resource', function() {
         it('should create a new resource from req.body', function(done) {
-            request
-                .post('/users')
-                .set('Accept', 'application/json')
-                .send({ givenname: 'Rick', lastname: 'Astley' })
-                .expect(200)
-                .end(function(err, result) {
-                    if (err) { done(err); }
-                    assert.equal(result.body, '/users/3');
-                    done();
+            User.count().then(function(len) {
+                request
+                    .post('/users')
+                    .set('Accept', 'application/json')
+                    .send({ givenname: 'Rick', lastname: 'Astley' })
+                    .expect(200)
+                    .end(function(err, result) {
+                        if (err) { done(err); }
+                        assert.equal(result.body, '/users/' + (len + 1));
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe('   PUT /:resource', function() {
+        it('should bulk update resources as defined in req.body', function(done) {
+            var foo = {id: 1, givenname: 'F', lastname: 'oo'}
+              , bar = {id: 2, givenname: 'B', lastname: 'ar'};
+
+            User.findAll().then(function(raw) {
+                var results = _.map(raw, function(r) {
+                    return clean(r.dataValues);
                 });
+
+                assert.deepEqual(users.dominik, _.findWhere(results, {id:foo.id}));
+                assert.deepEqual(users.hanna, _.findWhere(results, {id:bar.id}));
+
+                request
+                    .put('/users')
+                    .set('Accept', 'application/json')
+                    .send([foo, bar])
+                    .expect(200)
+                    .end(function(err) {
+                        if (err) { done(err); }
+                        User.findAll().then(function(all) {
+                            var actuals = _.map(all, function(r) {
+                                return clean(r.dataValues);
+                            });
+                            assert.deepEqual(foo, _.findWhere(actuals, {id: foo.id}));
+                            assert.deepEqual(bar, _.findWhere(actuals, {id: bar.id}));
+                            done();
+                        });
+                    });
+            });
         });
     });
 
@@ -210,8 +228,10 @@ describe('', function() {
                 .expect(200)
                 .end(function(err, result) {
                     if (err) { done(err); }
-                    assert.deepEqual(result.body, _.omit(users[0].dataValues, ['createdAt', 'updatedAt']));
-                    done();
+                    User.findOne(1).then(function(res) {
+                        assert.deepEqual(result.body, clean(res.dataValues));
+                        done();
+                    });
                 });
         });
     });
@@ -258,10 +278,12 @@ describe('', function() {
 
     describe('  POST /:resource/:id', function() {
         it('should return an error as this is not allowed', function(done) {
+            var rick = { givenname: 'Rick', lastname: 'Astley' };
+
             request
                 .post('/users/1')
                 .set('Accept', 'application/json')
-                .send({ givenname: 'Rick', lastname: 'Astley' })
+                .send(rick)
                 .expect(400)
                 .end(function(err, result) {
                     if (err) { done(err); }
@@ -274,10 +296,12 @@ describe('', function() {
 
     describe('   PUT /:resource/:id', function() {
         it('should update the resource with the data from req.body', function(done) {
+            var rick = { givenname: 'Rick', lastname: 'Astley' };
+
             request
                 .put('/users/1')
                 .set('Accept', 'application/json')
-                .send({ givenname: 'Rick', lastname: 'Astley' })
+                .send(rick)
                 .expect(200)
                 .end(function(err, result) {
                     if (err) { done(err); }
@@ -288,7 +312,7 @@ describe('', function() {
                         .expect(200)
                         .end(function(e, r) {
                             if (e) { done(e); }
-                            assert.deepEqual(r.body, { givenname: 'Rick', lastname: 'Astley', id: 1 });
+                            assert.deepEqual(r.body, _.extend({ id: 1 }, rick));
                             done();
                         });
                 });
@@ -308,6 +332,22 @@ describe('', function() {
                         done();
                     });
                 });
+        });
+    });
+
+    _.each(_.values(expressRestOrmErrors), function(error) {
+        describe('   GET /_errors/' + error.slug, function() {
+            it('should inform in detail about "' + error.error.reason + '"', function(done) {
+                request
+                    .get('/_errors/' + error.slug)
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .end(function(err, result) {
+                        if (err) { done(err); }
+                        assert.deepEqual(result.body, error.description);
+                        done();
+                    });
+            });
         });
     });
 });
