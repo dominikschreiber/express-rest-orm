66b4296c4edf945f4be798bd8998b20a3a1c7722
Dominik Schreiber <dschreiber@kellerkinder.de>
2015-05-25 13:06:50 +0200

    moved sources to src/main as es6 syntax was integrated

--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var _ = require('lodash')
-  , untyped = require('untyped')
-  , Q = require('q')
-  , errors = require('./errors')
-  , yaml = require('yamljs')
-  , EasyXml = require('easyxml')
-  , Promise = require('promise')
-  , xmldefaults = {
+import * as _ from 'lodash';
+import * as untyped from 'untyped';
+import * as Q from 'q';
+import * as yaml from 'yamljs';
+import * as EasyXml from 'easyxml';
+import {Router} from 'express';
+
+import * as errors from './errors';
+
+var xmldefaults = {
         singularizeChildren: true,
         allowAttributes: true,
         manifest: true
@@ -32,71 +34,70 @@ var _ = require('lodash')
  * for examples in this class, assume it is
  * mounted at '/api'
  */
-module.exports = function(models) {
+module.exports = models => {
     /**
      * creates the {{pseudo-mime}} => {{serialization}}
      * mappings required by the res.format of express
      */
-    function format(xml, res, object) {
+    let format = (xml, res, object) => {
         return {
-            xml: function() {
+            xml: () => {
                 res
                     .set('Content-Type', 'application/xml')
                     .send(xml.render(object)); 
             },
-            json: function() {
+            json: () => {
                 res
                     .json(object); 
             },
-            'text/x-yaml': function() {
+            'text/x-yaml': () => {
                 res
                     .set('Content-Type', 'text/x-yaml')
                     .send(yaml.stringify(object));
             }
         };
-    }
+    };
+
+    /**
+     * turns sequelize item to plain javascript object,
+     * omits `createdAt` and `updatedAt` values
+     */
+    let cleanitem = item => {
+        return _.omit(item.dataValues, 'createdAt', 'updatedAt');
+    };
 
     /**
      * turns a list of sequelize items to plain javascript objects
      * @see #cleanitem(item)
      */
-    function cleanitems(items) {
-            return _.chain(items)
-                .map(function(item) { return cleanitem(item); })
-                .value();
-    }
-    /**
-     * turns sequelize item to plain javascript object,
-     * omits `createdAt` and `updatedAt` values
-     */
-    function cleanitem(item) {
-        return _.omit(item.dataValues, 'createdAt', 'updatedAt');
-    }
+    let cleanitems = items => {
+        return items.map(item => { return cleanitem(item); });
+    };
 
     /**
      * creates an xml renderer based on EasyXml
      * given a root element (normally model.getTableName())
      */
-    function xmlbuilder(root) {
-        return new EasyXml(_.extend({
+    let xmlbuilder = root => {
+        return new EasyXml.default(_.extend({
             rootElement: root
         }, xmldefaults));
-    }
+    };
 
-    function errorbuilder(req, err) {
+    let errorbuilder = (req, err) => {
         return _.extend({
             url: req.baseUrl + '/' + errorpathelement + '/' + err.slug
         }, err.error);
-    }
+    };
 
-    var api = require('express').Router();
+    let api = new Router();
 
     /**
      * support ?method=(POST|PUT|DELETE) by
      * overwriting req.method if this parameter
      * is set
      */
-    api.use(function(req, res, next) {
+    api.use((req, res, next) => {
         if ('method' in req.query && ['POST', 'PUT', 'DELETE'].indexOf(req.query.method) > -1) {
             req.method = req.query.method;
             delete req.query.method;
@@ -113,21 +114,19 @@ module.exports = function(models) {
      * ['/api/foo', '/api/bar']
      * ```
      */
-    api.get('/', function(req, res, next) {
+    api.get('/', (req, res, next) => {
         res.format(format(
             xmlbuilder('endpoints'),
             res,
-            _.map(models, function(model) {
-                return [req.baseUrl, model.getTableName()].join('/');
-            })
+            models.map(model => { return [req.baseUrl, model.getTableName()].join('/'); })
         ));
         next();
     });
 
     // ===== /_errors/:slug =============================================================
 
-    _.each(_.values(errors), function(err) {
-        api.get('/' + errorpathelement + '/' + err.slug, function(req, res, next) {
+    _.values(errors).forEach(err => {
+        api.get('/' + errorpathelement + '/' + err.slug, (req, res, next) => {
             res.format(format(
                 xmlbuilder('error'),
                 res,
@@ -140,42 +139,50 @@ module.exports = function(models) {
     /**
      * for examples, let `model` be `foo`
      */
-    _.each(models, function(model) {
-        function sendwithstatus(res, status, body) {
+    models.forEach(model => {
+        const collection = '/' + model.getTableName();
+        const resource = collection + '/:id';
+        const field = resource + '/:field';
+        const foreignkeys = _.pairs(model.attributes)
+                            .filter(attributeOptions => 'referencesKey' in attributeOptions[1])
+                            .map(attributeOptions => { return {
+                                attribute: attributeOptions[0],
+                                foreignkey: attributeOptions[1].referencesKey
+                            }; });
+        const xml = xmlbuilder(model.getTableName());
+
+        let sendwithstatus = (res, status, body) => {
             if (res.req.query.suppress_response_codes === 'true') {
                 _.extend(body, { status: status });
             } else {
                 res.status(status);
             }
             res.format(format(xml, res, body));
-        }
+        };
 
-        function resourceurl(req, id) {
-            return req.baseUrl + collection + '/' + id;
-        }
+        let resourceurl = (req, id) => req.baseUrl + collection + '/' + id;
 
-        function getcollection(req) {
-            var options = {
+        let getcollection = req => {
+            let options = {
                     limit: defaults.limit, 
                     offset: defaults.offset
-                }
-              , where = {};
+                };
+            let where = {};
 
             // ?limit=, ?offset=
-            _.each(_.keys(options), function(key) {
+            Object.keys(options).forEach(key => {
                 if (key in req.query) {
                     options[key] = parseInt(req.query[key]);
                 }
             });
 
             // ?:field=
-            _.each(_.keys(_.omit(model.attributes, 'createdAt', 'updatedAt')), function(attribute) {
-                _.each(['', '~', '|', '^', '$', '*'], function(match) {
-                    var queryparam = attribute + match
-                      , value;
+            Object.keys(_.omit(model.attributes, 'createdAt', 'updatedAt')).forEach(attribute => {
+                ['', '~', '|', '^', '$', '*'].forEach(match => {
+                    const queryparam = attribute + match;
 
                     if (queryparam in req.query) {
-                        value = req.query[queryparam];
+                        let value = req.query[queryparam];
 
                         switch (match) {
                             case '': // exact match
@@ -198,51 +205,49 @@ module.exports = function(models) {
                 options.where = where;
             }
 
-            return new Promise(function(resolve) {
+            return new Promise(resolve => {
                 // ?include_docs=true
                 if (req.query.include_docs == 'true') {
                     // ?fields=
                     if ('fields' in req.query) {
-                        options.attributes = _.keys(untyped.parse(req.query.fields)).concat(['id']);
+                        options.attributes = Object.keys(untyped.parse(req.query.fields)).concat(['id']);
                     }
-                    model.findAll(options).then(function(results) {
+                    model.findAll(options).then(results => {
                         resolve(cleanitems(results));
                     });
                 } else {
                     model.findAll(_.defaults({
                         attributes: ['id']
-                    }, options)).then(function(results) {
-                        resolve(_.map(results, function(result) {
-                            return resourceurl(req, result.id);
-                        }));
+                    }, options)).then(results => {
+                        resolve(results.map(result => resourceurl(req, result.id)));
                     });
                 }
             });
-        }
+        };
 
-        function getresource(req) {
-            var options = {}
-              , shouldIncludeDocs = req.query.include_docs == 'true';
+        let getresource = req => {
+            const shouldIncludeDocs = req.query.include_docs == 'true';
+            let options = {};
 
             // ?fields=
             if ('fields' in req.query) {
-                options.attributes = _.uniq(_.keys(untyped.parse(req.query.fields)).concat(['id']));
+                options.attributes = _.uniq(Object.keys(untyped.parse(req.query.fields)).concat(['id']));
             }
 
-            return new Promise(function(resolve, reject) {
-                model.findOne(req.params.id.replace(/\.[^\.]+$/g, ''), options).then(function(result) {
+            return new Promise((resolve, reject) => {
+                model.findOne(req.params.id.replace(/\.[^\.]+$/g, ''), options).then(result => {
                     // ?include_docs=true
                     if (shouldIncludeDocs) {
-                        Q.all(_.map(foreignkeys, function(foreignkey) {
-                            var foreigntable = foreignkey.foreignkey.split('.')[0]
-                                // can not use sequelize.model(...) as the model name
-                                // may not be derived from foreignkey.foreignkey
-                                // (e.g. foreignkey could be 'users.id', but model name is 'user')
-                              , foreignmodel = _.find(models, function(m) { return m.getTableName() === foreigntable; });
+                        Q.all(foreignkeys.map(foreignkey => {
+                            const foreigntable = foreignkey.foreignkey.split('.')[0];
+                            // can not use sequelize.model(...) as the model name
+                            // may not be derived from foreignkey.foreignkey
+                            // (e.g. foreignkey could be 'users.id', but model name is 'user')
+                            const foreignmodel = _.find(models, m => m.getTableName() === foreigntable);
 
                             return foreignmodel.findOne(result.dataValues[foreignkey.attribute]);
-                        })).then(function(foreignvalues) {
-                            _.each(_.zip(foreignkeys, foreignvalues), function(keyValue) {
+                        })).then(foreignvalues => {
+                            _.zip(foreignkeys, foreignvalues).forEach(keyValue => {
                                 result.dataValues[keyValue[0].attribute] = cleanitem(keyValue[1]);
                             });
                             resolve(cleanitem(result));
@@ -250,27 +255,14 @@ module.exports = function(models) {
                     }
                     // urls for foreign keys
                     else {
-                        _.each(foreignkeys, function(foreignkey) {
-                            result.dataValues[foreignkey.attribute] = req.baseUrl + '/' + foreignkey.foreignkey.split('.')[0] + '/' + result.dataValues[foreignkey.attribute];
-                        });
+                        foreignkeys.forEach(foreignkey => 
+                            result.dataValues[foreignkey.attribute] = req.baseUrl + '/' + foreignkey.foreignkey.split('.')[0] + '/' + result.dataValues[foreignkey.attribute]
+                        );
                         resolve(cleanitem(result));
                     }
                 }, reject);
             });
-        }
-
-        var collection = '/' + model.getTableName()
-          , resource = collection + '/:id'
-          , field = resource + '/:field'
-          , foreignkeys = _.chain(_.pairs(model.attributes))
-                            .filter(function(attributeOptions) {
-                                return 'referencesKey' in attributeOptions[1];
-                            })
-                            .map(function(attributeOptions) {
-                                return { attribute: attributeOptions[0], foreignkey: attributeOptions[1].referencesKey };
-                            })
-                            .value()
-          , xml = xmlbuilder(model.getTableName());
+        };
 
         // ===== /:resource =============================================================
 
@@ -287,16 +279,16 @@ module.exports = function(models) {
          * [{id: 1, name: goo}, {id: 2, name: gle}]
          * ```
          */
-        api.get(collection, function(req, res, next) {
-            getcollection(req).then(function(items) {
+        api.get(collection, (req, res, next) => {
+            getcollection(req).then(items => {
                 res.format(format(xml, res, items));
                 next();
             });
         });
 
-        api.get(collection + '.:ext', function(req, res, next) {
+        api.get(collection + '.:ext', (req, res, next) => {
             if (req.params.ext in extensionmappings) {
-                getcollection(req).then(function(items) {
+                getcollection(req).then(items => {
                     format(xml, res, items)[extensionmappings[req.params.ext]]();
                     next();
                 });
@@ -306,8 +298,8 @@ module.exports = function(models) {
             }
         });
 
-        api.post(collection, function(req, res, next) {
-            model.create(req.body).then(function(resource) {
+        api.post(collection, (req, res, next) => {
+            model.create(req.body).then(resource => {
                 res.format(format(
                     xml,
                     res,
@@ -317,17 +309,15 @@ module.exports = function(models) {
             });
         });
 
-        api.put(collection, function(req, res, next) {
-            Q.all(_.map(req.body, function(item) {
-                return model.upsert(item);
-            })).then(function() {
+        api.put(collection, (req, res, next) => {
+            Q.all(req.body.map(item => model.upsert(item))).then(() => {
                 res.sendStatus(200);
                 next();
             });
         });
 
-        api.delete(collection, function(req, res, next) {
-            model.destroy({where: {id: {gt: 0}}}).then(function() {
+        api.delete(collection, (req, res, next) => {
+            model.destroy({where: {id: {gt: 0}}}).then(() => {
                 res.sendStatus(200);
                 next();
             });
@@ -340,21 +330,21 @@ module.exports = function(models) {
          *
          * {id: 1, name: 'goo'}
          */
-        api.get(resource, function(req, res, next) {
+        api.get(resource, (req, res, next) => {
             // /:id matches /:id.:ext as well -> next() in that case
             if (/\.[^\.]+$/.test(req.params.id)) {
                 next();
             } else {
-                getresource(req).then(function(item) {
+                getresource(req).then(item => {
                     res.format(format(xml, res, item));
                     next();
                 });
             }
         });
 
-        api.get(resource + '.:ext', function(req, res, next) {
+        api.get(resource + '.:ext', (req, res, next) => {
             if (req.params.ext in extensionmappings) {
-                getresource(req).then(function(item) {
+                getresource(req).then(item => {
                     format(xml, res, item)[extensionmappings[req.params.ext]]();
                     next();
                 });
@@ -364,20 +354,20 @@ module.exports = function(models) {
             }
         });
 
-        api.post(resource, function(req, res, next) {
+        api.post(resource, (req, res, next) => {
             sendwithstatus(res, 400, errorbuilder(req, errors.POST_RESOURCE));
             next();
         });
 
-        api.put(resource, function(req, res, next) {
-            model.update(req.body, {where: {id: req.params.id}}).then(function() {
+        api.put(resource, (req, res, next) => {
+            model.update(req.body, {where: {id: req.params.id}}).then(() => {
                 res.format(format(xml, res, resourceurl(req, req.params.id)));
                 next();
             });
         });
 
-        api.delete(resource, function(req, res, next) {
-            model.destroy({where: {id: req.params.id}}).then(function() {
+        api.delete(resource, (req, res, next) => {
+            model.destroy({where: {id: req.params.id}}).then(() => {
                 res.format(format(xml, res, req.baseUrl + collection));
                 next();
             });
@@ -387,13 +377,13 @@ module.exports = function(models) {
 
         // ----- /:resource/:id/:field --------------------------------------------------
 
-        api.get(field, function(req, res, next) {
+        api.get(field, (req, res, next) => {
             // /:field matches /:field.:ext as well -> next() in that case
             if (/\.[^\.]+$/.test(req.params.field)) {
                 next();
             } else {
                 if (req.params.field in model.attributes) {
-                    getresource(req).then(function(resource) {
+                    getresource(req).then(resource => {
                         if ('referencesKey' in model.attributes[req.params.field]) {
                             res.redirect(resource[req.params.field]);
                         } else {
@@ -410,12 +400,11 @@ module.exports = function(models) {
 
         // ----- /:resource/:id/:field.:ext ---------------------------------------------
 
-        api.get(field + '.:ext', function(req, res, next) {
-            var field;
+        api.get(field + '.:ext', (req, res, next) => {
             if (req.params.ext in extensionmappings) {
-                field = req.params.field.replace(/\.[^\.]+$/g, '');
+                let field = req.params.field.replace(/\.[^\.]+$/g, '');
                 if (field in model.attributes) {
-                    getresource(req).then(function(resource) {
+                    getresource(req).then(resource => {
                         if ('referencesKey' in model.attributes[field]) {
                             res.redirect(resource[field]);
                         } else {
