606d2ba545c6c3b97b151830adc6d6364959afab
Dominik Schreiber <dschreiber@kellerkinder.de>
2015-05-25 14:45:00 +0200

    switched from blanket.js to istanbul with grunt-mocha-istanbul task

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,24 +32,56 @@ module.exports = function(grunt) {
 
         // ===== test =========================================================
 
-        mochaTest: {
-            test: {
-                src: 'dist/test/**/*.js',
+        mocha_istanbul: {
+            coverage: {
+                src: 'dist/test',
                 options: {
-                    reporter: 'spec'
+                    mask: '**/*.js'
+                }
+            },
+            coveralls: {
+                src: 'dist/test',
+                options: {
+                    coverage: true,
+                    check: {
+                        lines: 100,
+                        statements: 100
+                    },
+                    root: 'dist/main',
+                    reportFormats: ['cobertura', 'lcovonly']
+                }
+            },
+            istanbul_check_coverage: {
+                default: {
+                    options: {
+                        coverageFolder: 'coverage',
+                        check: {
+                            lines: 100,
+                            statements: 100
+                        }
+                    }
                 }
             }
         }
-        // "test": "multi='spec=- travis-cov=-' ./node_modules/.bin/mocha --require blanket --timeout 4000 --reporter mocha-multi",
+    });
+
+    grunt.event.on('coverage', function(lcovFileContents, done) {
+        require('coveralls').handleInput(lcovFileContents, function(err) {
+            if (err) { return done(err); }
+            done();
+        });
     });
 
     grunt.registerTask('validate', ['clean', 'jshint']);
 
     grunt.registerTask('compile', ['validate', 'babel']);
 
-    grunt.registerTask('test', ['compile', 'mochaTest']);
+    grunt.registerTask('test', ['compile', 'mocha_istanbul:coverage']);
 
     grunt.registerTask('install', ['test']);
 
     grunt.registerTask('default', ['install']);
+
+    // for travis build
+    grunt.registerTask('travis', ['mocha_istanbul:coveralls']);
 };
